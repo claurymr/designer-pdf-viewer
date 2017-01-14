74e42e7392dae4d11cbf1125939d41455b64d3b5
mejiarod <mejiarod@msu.edu>
2017-01-14 01:04:11 -0500

    Read word given from input. Added loop to iterate through word chars and calculated char with max height, total number of chars. Codeadd . totally functional.

--- main.cpp
+++ main.cpp
@@ -8,23 +8,42 @@
 using namespace std;
 
 #define ENGLISH_LETTERS 26
+#define LETTER_WIDTH 1 //Width of a word is 1mm
 
 int main()
 {
-    char letters[ENGLISH_LETTERS] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                                      'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char letters[ENGLISH_LETTERS] = {'a', 'b', 'c', 'd', 'e', 'f',
+                                     'g', 'h', 'i', 'j', 'k', 'l',
+                                     'm', 'n', 'o', 'p', 'q', 'r',
+                                     's', 't', 'u', 'v', 'w', 'x',
+                                     'y', 'z'};
     map<char, int> heights;
     int height = 0;
     int maxHeight = 0;
     string word = "";
+    int wordCount = 0;
+    int result = 0;
 
     for(int i = 0; i < ENGLISH_LETTERS; i++){
         cin >> height;
         heights.insert(pair<char, int>(letters[i], height));
     }
 
-    for(auto set: heights){
-        cout << "Set: " << set.first << " " << set.second << endl;
+    cin >> word;
+
+    for(auto c: word){
+        char ch = c;
+        int h = heights.find(ch)->second;
+
+        if(h > maxHeight)
+            maxHeight = h;
+
+        wordCount++;
     }
+
+    result = wordCount * LETTER_WIDTH * maxHeight;
+
+    cout << result;
+
     return 0;
 }
